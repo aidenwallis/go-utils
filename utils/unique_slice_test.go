3d10f5c269b9438d59c11b93219c937c61a8f150
Aiden <aiden@aidenwallis.co.uk>
2022-04-24 20:17:13 +0100

    actually rename the func in tests

--- utils/unique_slice_test.go
+++ utils/unique_slice_test.go
@@ -15,7 +15,7 @@ func TestUniqueSlice(t *testing.T) {
 
 		input := []int{1, 2, 3, 4, 5, 1, 2, 2, 6, 6}
 		output := []int{1, 2, 3, 4, 5, 6}
-		assert.Equal(t, output, utils.UniqueSliceValues(input))
+		assert.Equal(t, output, utils.UniqueSlice(input))
 	})
 
 	t.Run("strings", func(t *testing.T) {
@@ -23,6 +23,6 @@ func TestUniqueSlice(t *testing.T) {
 
 		input := []string{"a", "b", "A", "b", "C", "d", "E"}
 		output := []string{"a", "b", "A", "C", "d", "E"}
-		assert.Equal(t, output, utils.UniqueSliceValues(input))
+		assert.Equal(t, output, utils.UniqueSlice(input))
 	})
 }
