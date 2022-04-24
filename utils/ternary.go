ba5e0d719d6397754f83a4f9557934d6119a846d
Aiden <aiden@aidenwallis.co.uk>
2022-04-24 20:03:35 +0100

    make ternary type parameter any (#6)

--- utils/ternary.go
+++ utils/ternary.go
@@ -1,7 +1,7 @@
 package utils
 
 // Ternary allows you to write ternary expressions in go, if exp is true, then ifCond is returned, else, elseCond is returned.
-func Ternary[T comparable](exp bool, ifCond T, elseCond T) T {
+func Ternary[T any](exp bool, ifCond T, elseCond T) T {
 	if exp {
 		return ifCond
 	}
