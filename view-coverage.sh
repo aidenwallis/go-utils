892e6b1db03e574da3fea2de197a9e60ddc0882c
Aiden <aiden@aidenwallis.co.uk>
2022-04-24 19:34:25 +0100

    Add credit for the person who made the view-coverage script (#3)

--- view-coverage.sh
+++ view-coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # This small script simply runs the tests and opens a browser window of the results.
-# From: 
+# From: https://stackoverflow.com/a/27284510
 
 t="/tmp/go-cover.$$.tmp"
 go test -coverprofile=$t -race ./... $@ && go tool cover -html=$t && unlink $t
