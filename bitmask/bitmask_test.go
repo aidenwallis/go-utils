8cdb05a14ed9643f20a1da6cd3d68fc51f7e164c
Aiden <12055114+aidenwallis@users.noreply.github.com>
2022-11-11 01:36:51 +0000

    Add bitmask utils

---
+++ bitmask/bitmask_test.go
@@ -0,0 +1,23 @@
+package bitmask_test
+
+import (
+	"testing"
+
+	"github.com/aidenwallis/go-utils/bitmask"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBits(t *testing.T) {
+	t.Parallel()
+
+	const (
+		a = 1 << 0
+		b = 1 << 1
+		c = 1 << 2
+	)
+
+	assert.Equal(t, a|b, bitmask.Add(a, b))
+	assert.Equal(t, a, bitmask.Remove(a|b, b))
+	assert.True(t, bitmask.Has(a|b, b))
+	assert.False(t, bitmask.Has(a|b, c))
+}
