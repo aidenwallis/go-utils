7293bb76838c5bbd5f5d64a2a01b2718bdebfeea
Aiden <12055114+aidenwallis@users.noreply.github.com>
2023-03-20 00:51:48 +0000

    ensure test hits edge case

--- utils/chunk_slice_test.go
+++ utils/chunk_slice_test.go
@@ -10,13 +10,13 @@ import (
 func TestChunkSlice(t *testing.T) {
 	t.Parallel()
 
-	in := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	expected := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	in := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	expected := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8}}
 
 	out := utils.ChunkSlice(in, 3)
 
 	for i, chunk := range out {
-		assert.Equal(t, 3, len(chunk))
+		assert.Equal(t, len(expected[i]), len(chunk))
 		for j, v := range chunk {
 			assert.Equal(t, expected[i][j], v)
 		}
